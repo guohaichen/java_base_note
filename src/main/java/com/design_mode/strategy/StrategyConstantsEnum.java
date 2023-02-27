7a370aa8d6858fa86a2ae84e136aa665bbe761a1
guohai <1299117865@qq.com>
2023-02-27 14:59:49 +0800

    notes update

--- src/main/java/com/design_mode/strategy/StrategyConstantsEnum.java
+++ src/main/java/com/design_mode/strategy/StrategyConstantsEnum.java
@@ -3,7 +3,7 @@ package com.design_mode.strategy;
 /**
  * @author cgh
  * @create 2023-02-23 13:47
- *
+ *策略枚举类
  */
 public enum StrategyConstantsEnum {
     ADDITION_STRATEGY,
--- src/main/java/com/design_mode/strategy/StrategyFactory.java
+++ src/main/java/com/design_mode/strategy/StrategyFactory.java
@@ -5,16 +5,23 @@ import java.util.HashMap;
 /**
  * @author cgh
  * @create 2023-02-23 13:43
+ * 策略工厂类，用于创建合适的策略。
+ * 这里省去了用if,else这种判断，使用map代替了，更为简洁。
  */
 public class StrategyFactory {
-    private static final HashMap<StrategyConstantsEnum,Strategy> strategyMap = new HashMap<>();
+    private static final HashMap<StrategyConstantsEnum, Strategy> strategyMap = new HashMap<>();
+
     static {
-        strategyMap.put(StrategyConstantsEnum.ADDITION_STRATEGY,new AdditionStrategy());
-        strategyMap.put(StrategyConstantsEnum.SUBTRACTION_STRATEGY,new SubtractionStrategy());
-        strategyMap.put(StrategyConstantsEnum.DIVISION_STRATEGY,new DivisionStrategy());
+        strategyMap.put(StrategyConstantsEnum.ADDITION_STRATEGY, new AdditionStrategy());
+        strategyMap.put(StrategyConstantsEnum.SUBTRACTION_STRATEGY, new SubtractionStrategy());
+        strategyMap.put(StrategyConstantsEnum.DIVISION_STRATEGY, new DivisionStrategy());
     }
 
-    public static Strategy getStrategy(StrategyConstantsEnum strategyType){
+    //工厂对外暴露的get方法。
+    public static Strategy getStrategy(StrategyConstantsEnum strategyType) {
+        if (strategyType == null) {
+            throw new RuntimeException("strategy should not be null!");
+        }
         return strategyMap.get(strategyType);
     }
 }
